fb60f449cc68ce18eb6faeb914682ea9e901af89
Gustavo Rodrigues <gustavo.oliveira8258@gmail.com>
2021-08-08 17:16:32 -0300

    Config Identity

--- src/DevIO.Api/Startup.cs
+++ src/DevIO.Api/Startup.cs
@@ -34,6 +34,8 @@ namespace DevIO.Api
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
             });
 
+            services.AddIdentityConfiguration(Configuration);
+
             services.AddAutoMapper(typeof(Startup));
 
             services.WebApiConfig();
@@ -54,6 +56,7 @@ namespace DevIO.Api
                 app.UseHsts();
             }
 
+            app.UseAuthentication();
             app.UseMvcConfiguration();
         }
     }
